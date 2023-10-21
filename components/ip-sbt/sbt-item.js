0c04e3c8e270fa19dfa2d2646ed6505f7d8cd13f
dev-chang-won <dev.chang12@gmail.com>
2023-10-21 19:53:06 +0900

    [수정]화면처리

--- components/ip-sbt/sbt-item.js
+++ components/ip-sbt/sbt-item.js
@@ -3,13 +3,13 @@ import {useState} from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-//pdfjs.GlobalWorkerOptions.workerSrc = new URL( 'pdfjs-dist/build/pdf.worker.min.js', import.meta.url, ).toString();
 
 export default function SbtItem({data}){
 
     const [numPages, setNumPages] = useState ();
     const [pageNumber, setPageNumber] = useState (1);
-  
+    const [pageScale, setPageScale] = useState(1); // 페이지 스케일
+
     function onDocumentLoadSuccess({ numPages }) {
       setNumPages(numPages);
     }
@@ -28,16 +28,13 @@ export default function SbtItem({data}){
 	const ip_info_03 = data.ip_info_03    
     const tokenUri = data?.token_uri
     return (
-        <div className="project-card">
+        <div className="project-card content-center">
             {/* 생성된 PDF null 체크 */}
             {tokenUri && (
                 <div onContextMenu={e => e.preventDefault()}>
                     <Document file={tokenUri} onLoadSuccess={onDocumentLoadSuccess}>
-                        <Page width={50} height={100} pageNumber={pageNumber} />
+                        <Page width={150} height={72} scale={3} pageNumber={1} />
                     </Document>
-                    <p>
-                    Page {pageNumber} of {numPages}
-                    </p>
                 </div>                
             )}
             <div className="p-4 flex flex-col">
